76f3bc2208feb92452990b40ee8ac7a765da3e54
Matt Browne <mbrowne@artnet.com>
2020-05-31 22:36:25 -0400

    made dev server startup output more user-friendly

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,18 +1,29 @@
-const webpack = require('webpack');
-
-const commonPaths = require('./paths');
+const webpack = require('webpack')
+const chalk = require('chalk')
+const commonPaths = require('./paths')
 
 module.exports = {
-  mode: 'development',
-  output: {
-    filename: '[name].js',
-    path: commonPaths.outputPath,
-    chunkFilename: '[name].js',
-  },
-  devServer: {
-    contentBase: commonPaths.outputPath,
-    compress: true,
-    hot: true,
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()],
-};
+    mode: 'development',
+    output: {
+        filename: '[name].js',
+        path: commonPaths.outputPath,
+        chunkFilename: '[name].js',
+    },
+    devServer: {
+        contentBase: commonPaths.outputPath,
+        compress: true,
+        hot: true,
+        onListening: server => {
+            const { port } = server.listeningApp.address()
+            server.compiler.hooks.done.tap('done', () => {
+                setImmediate(() => {
+                    console.log()
+                    console.log(
+                        chalk.cyan.bold(`Running at http://localhost:${port}`)
+                    )
+                })
+            })
+        },
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+}
